c82ca210d5d5aee3621c550efcc6587d2b6539ad
m0su <sirm0su@gmail.com>
2021-09-01 18:53:45 +0900

    Initial Commit

---
+++ Shared/SwiftUI_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PlaygroundApp.swift
+//  Shared
+//
+//  Created by seo on 2021/09/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
